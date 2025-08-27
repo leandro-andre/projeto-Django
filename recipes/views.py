be55c47ef35d3b03439f74cab0d774d9a19d7204
Leandro André <leandr.andre.1997@gmail.com>
2025-08-27 10:52:16 -0300

    Mundando variaveis

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 
 
 def home (request):
-    return render (request,'recipes/pages/home.html')
+    return render (request,'recipes/pages/home.html', context= {
+        'recipes': [make_recipe() for _ in range(12)],
+    })
 
 def recipe (request, id):
     return render (request,'recipes/pages/recipe-view.html')
